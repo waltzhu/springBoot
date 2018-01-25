e3faf03e80d592f1cec357699f5914e162c47737
WaltZhu <zhujunsheng80@126.com>
2018-01-25 13:45:03 +0800

    20180125-waltzhu-spring boot hello world

---
+++ SpringMVC_Boot/src/main/java/com/waltzhu/demo/SpringMvcBootApplication.java
@@ -0,0 +1,21 @@
+package com.waltzhu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/*
+ * @SpringBootApplication组合了3个注解：
+ * @Configration 标明该类使用spring基于java的配置
+ * @ComponentScan 组件扫描
+ * @EnableAutoConfiguration
+ * (exclude={DataSourceAutoConfiguration.class,})
+ */
+
+@SpringBootApplication
+public class SpringMvcBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcBootApplication.class, args);
+	}
+}
