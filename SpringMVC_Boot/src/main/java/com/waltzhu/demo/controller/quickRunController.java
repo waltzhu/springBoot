0719a9a97a4a73183a5df1715a3b871f29081f16
WaltZhu <zhujunsheng80@126.com>
2018-12-04 16:40:55 +0800

    spring session共享

--- SpringMVC_Boot/src/main/java/com/waltzhu/demo/controller/quickRunController.java
+++ SpringMVC_Boot/src/main/java/com/waltzhu/demo/controller/quickRunController.java
@@ -1,7 +1,5 @@
 package com.waltzhu.demo.controller;
 
-import java.util.UUID;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -18,7 +16,7 @@ public class quickRunController {
 	    private RedisService redisService ;
 
 
-	    @RequestMapping(value = "/test",method = RequestMethod.GET)
+	    @RequestMapping(value = "/saveredis",method = RequestMethod.GET)
 	    public void demoTest(){
 	        redisService.set("2","value22222");
 	    } 
@@ -31,24 +29,9 @@ public class quickRunController {
 			System.out.println("main sessionId:" + sessionId);
 			return "main";
 		} else { // sessionIdÎª¿Õ
-			return "redirect:/login";
+			return "login";
 		}
 	}
-	
-	
-	@RequestMapping(value="/login", method=RequestMethod.GET)
-	public String login() {
-		return "login";
-	}
-	
-	@RequestMapping(value="/doLogin", method=RequestMethod.POST)
-	public String doLogin(HttpServletRequest request) {
-		System.out.println("I do real login here");
-		HttpSession session = request.getSession();
-		String sessionId = UUID.randomUUID().toString();
-		session.setAttribute("sessionId", sessionId);
-		System.out.println("login sessionId:" + sessionId);
-		return "redirect:/main";
-	} 
+	 
 	
 }
